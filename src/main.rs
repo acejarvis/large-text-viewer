e477b9890661e249297f0bf56757f947e0ac0188
webbrain-one <295484252+webbrain-one@users.noreply.github.com>
2026-08-01 03:49:46 +0300

    Accept file path as argument
    This enables opening files via command line arguments. This allows the
    application to be set as a default editor and supports %F or %U in desktop
    shortcuts.
    
    Fixes #29

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,17 @@ mod app;
 
 use app::TextViewerApp;
 use eframe::egui;
+use std::env;
+use std::path::PathBuf;
 
 fn main() -> eframe::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let initial_file: Option<PathBuf> = if args.len() > 1 {
+        Some(PathBuf::from(&args[1]))
+    } else {
+        None
+    };
+
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([1200.0, 800.0])
@@ -14,6 +23,6 @@ fn main() -> eframe::Result<()> {
     eframe::run_native(
         "Large Text Viewer",
         options,
-        Box::new(|_cc| Ok(Box::new(TextViewerApp::default()))),
+        Box::new(move |_cc| Ok(Box::new(TextViewerApp::new(initial_file.clone())))),
     )
 }
